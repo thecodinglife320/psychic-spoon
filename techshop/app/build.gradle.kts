f5960d4cb0508684e8afafb34050ff25047f68b0
thecodinglife <ttll442003@gmail.com>
2024-12-02 00:26:41 +0700

    IntroduceFragment

--- techshop/app/build.gradle.kts
+++ techshop/app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.firebase.analytics)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
